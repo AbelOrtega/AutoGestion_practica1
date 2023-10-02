b29afb349b8850db607ca0832102ec2b2c1a4438
AbelOrtega <146675783+AbelOrtega@users.noreply.github.com>
2023-10-02 00:45:49 -0300

    Add files via upload

---
+++ app/Models/Gasto.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gasto extends Model
+{
+    use HasFactory;
+
+    public function categoria()
+    {
+        return $this->hasOne('App\Models\Categoria');
+    }
+
+    public function item()
+    {
+        return $this->hasMany('App\Models\Item');
+    }
+
+    public function pago()
+    {
+        return $this->hasOne('App\Models\Pago');
+    }
+
+    public function etiqueta()
+    {
+        return $this->belongsToMany('App\Models\Etiqueta');
+    }
+}
